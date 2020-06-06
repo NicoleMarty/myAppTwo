c90785ad5667e2a72384ad4227bfdb6dc4b5e8d6
Nicole Marty <nmarty310@gmail.com>
2020-06-06 16:19:20 -0500

    setup firebase config for database and hosting

--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -14,7 +14,7 @@ const Tab1: React.FC = () => {
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Tab 1</IonTitle>
+          <IonTitle>Left Brain</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent>
@@ -23,7 +23,7 @@ const Tab1: React.FC = () => {
             <IonTitle size='large'>Tab 1</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <TabOneContainer name='Tab 1 page' />
+        <TabOneContainer name='Login/Sign Up' />
       </IonContent>
     </IonPage>
   )
