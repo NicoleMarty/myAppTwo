dfad4c8307b3c274c39f1d0b96cbfbb1247480f5
Nicole Marty <nmarty310@gmail.com>
2020-06-08 11:39:16 -0500

    starting to setup login

--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -1,15 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {
   IonContent,
   IonHeader,
   IonPage,
   IonTitle,
-  IonToolbar
+  IonToolbar,
+  IonList,
+  IonItem,
+  IonInput,
+  IonButton
 } from '@ionic/react'
 import TabOneContainer from '../components/TabOneContainer'
 import './Tab1.css'
 
 const Tab1: React.FC = () => {
+  const [text, setText] = useState<string>()
+  const [password, setPassword] = useState<string>()
   return (
     <IonPage>
       <IonHeader>
@@ -24,6 +30,29 @@ const Tab1: React.FC = () => {
           </IonToolbar>
         </IonHeader>
         <TabOneContainer name='Login/Sign Up' />
+        <IonList>
+          <IonItem>
+            <IonInput
+              type='email'
+              value={text}
+              placeholder='Email'
+              onIonChange={e => setText(e.detail.value!)}
+            ></IonInput>
+          </IonItem>
+          <IonItem>
+            <IonInput
+              type='password'
+              value={password}
+              placeholder='Password'
+              required
+              onIonChange={e => setPassword(e.detail.value!)}
+            ></IonInput>
+          </IonItem>
+
+          <IonButton type='submit' expand='block'>
+            Log In
+          </IonButton>
+        </IonList>
       </IonContent>
     </IonPage>
   )
